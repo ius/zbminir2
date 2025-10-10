ab85761e7daf6360455eef4c58215e4471f6a2f3
Gilbert J. M. Forkel <gilbert@erlangen.ccc.de>
2025-10-10 11:15:04 +0200

    Check for USB device, otherwise zigbee_z3_gateway will throw an missing arguments error

--- ota.sh
+++ ota.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-rlwrap ./build/zigbee_z3_gateway.elf -n 0 -d build -p $(readlink -f /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0)
+DEV=/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0
+
+if [ -e $DEV ]
+then
+	rlwrap ./build/zigbee_z3_gateway.elf -n 0 -d build -p $(readlink -f $DEV)
+else
+	echo Zigbee USB adapter not present, expecting $DEV
+fi
+
